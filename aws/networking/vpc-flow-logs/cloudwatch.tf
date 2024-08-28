37b43336b413b6261979eeca237db6acc210a3c0
Robert Sturla <robertsturla@outlook.com>
2024-08-28 23:38:54 +0100

    feat(networking): create VPC Flow Logs networking module

---
+++ aws/networking/vpc-flow-logs/cloudwatch.tf
@@ -0,0 +1,55 @@
+resource "aws_cloudwatch_log_group" "logs" {
+  count = local.enable_cloudwatch_destination ? 1 : 0
+
+  name_prefix       = "${var.name}-flow-logs"
+  retention_in_days = var.cloudwatch_log_retention_in_days
+
+  tags = var.tags_all
+}
+
+resource "aws_iam_role" "cloudwatch" {
+  count = local.enable_cloudwatch_destination ? 1 : 0
+
+  name_prefix        = "${var.name}-flow-logs"
+  assume_role_policy = data.aws_iam_policy_document.cloudwatch_assume_role[0].json
+
+  tags = var.tags_all
+}
+
+resource "aws_iam_policy" "cloudwatch_iam_policy" {
+  count = local.enable_cloudwatch_destination ? 1 : 0
+
+  name_prefix = "allow-publish-flow-logs-"
+  policy      = data.aws_iam_policy_document.cloudwatch_iam_policy[0].json
+
+  tags = var.tags_all
+}
+
+data "aws_iam_policy_document" "cloudwatch_assume_role" {
+  count = local.enable_cloudwatch_destination ? 1 : 0
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "cloudwatch_iam_policy" {
+  count = local.enable_cloudwatch_destination ? 1 : 0
+
+  statement {
+    sid    = "AllowPublishToCloudWatchLogs"
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+    resources = ["${aws_cloudwatch_log_group.logs[0].arn}:*"]
+  }
+}
