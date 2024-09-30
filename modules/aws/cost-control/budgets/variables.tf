224a7c00606ffe1b5b269cd004b69cda357040ec
Robert Sturla <robertsturla@outlook.com>
2024-10-01 00:41:16 +0100

    fix(cost-control): budgets cost filter not required (#46)
    * fix(cost-control): budgets cost filter shouldn't be a requried field
    
    * fix(cost-control): cost_filter default value is a list

--- modules/aws/cost-control/budgets/variables.tf
+++ modules/aws/cost-control/budgets/variables.tf
@@ -30,10 +30,10 @@ variable "budgets" {
       include_upfront            = false
       use_blended                = false
     })
-    cost_filter = list(object({
+    cost_filter = optional(list(object({
       name   = string
       values = list(string)
-    }))
+    })), [])
     tags = optional(map(string), {})
   }))
   default = {}
