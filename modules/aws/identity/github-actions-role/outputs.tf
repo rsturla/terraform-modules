f5a8040a398384c057ebbb7c235ad00cbd4173de
Robert Sturla <robertsturla@outlook.com>
2024-10-03 14:32:00 +0100

    fix(identity): remove policy_arns output

--- modules/aws/identity/github-actions-role/outputs.tf
+++ modules/aws/identity/github-actions-role/outputs.tf
@@ -7,8 +7,3 @@ output "name" {
   description = "The name of the IAM role"
   value       = aws_iam_role.this.name
 }
-
-output "policy_arns" {
-  description = "The ARNs of the IAM policies attached to the role"
-  value       = aws_iam_role_policy_attachment.this[*].policy_arn
-}
