7a02bf297711f53245406ccf96c18f16c0583d73
Robert Sturla <robertsturla@outlook.com>
2024-10-05 14:36:03 +0100

    chore(security): mark account names and emails as sensitive

--- modules/aws/security/organizations/outputs.tf
+++ modules/aws/security/organizations/outputs.tf
@@ -21,14 +21,19 @@ output "master_account_arn" {
 output "master_account_id" {
   description = "Identifier of the master account."
   value       = local.master_account_id
+  sensitive   = true
 }
 
 output "master_account_email" {
   description = "Email address of the master account."
   value       = local.master_account_email
+  sensitive   = true
 }
 
-output "child_accounts" {
-  description = "A map of all accounts created by this module (NOT including the root account). The keys are the names of the accounts and the values are the attributes for the account as defined in the aws_organizations_account resource."
-  value       = aws_organizations_account.child_accounts
+output "child_account_ids" {
+  description = "A map of all accounts created by this module (NOT including the root account). The keys are the names of the accounts and the values are the IDs."
+  value = {
+    for account in aws_organizations_account.child_accounts : account.name => account.id
+  }
+  sensitive   = true
 }
